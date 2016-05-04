3c427e0d9da3511cfe9434d7be92dd3b5e14c55c
Volodymyr Shelest <volodyashelest76@gmail.com>
2016-05-05 00:53:33 +0300

    commıt3
    механический карандаш

--- Accessories/Pen.php
+++ Accessories/Pen.php
@@ -7,7 +7,7 @@ class Pen extends WritingAccessories
     private $inkColor;
     private $activate = false;
 
-    public function __construct($inkColor, $widthLine, $bodyColor = "", $description = "", $manufacturer = "")
+    public function __construct($widthLine, $bodyColor = "", $description = "", $manufacturer = "", $inkColor = "gray")
     {
         parent::__construct( $bodyColor, $description, $manufacturer);
         $this->inkColor = $inkColor;
@@ -33,18 +33,15 @@ class Pen extends WritingAccessories
     {
         if($this->activate) {
             print("Write(line:$this->widthLine mm, color: $this->inkColor):$text <br>");
-        }else{
-            print("Automatic pen off <br>");
+            $this->activate = false;//lead decreased
+        } else {
+            print("Push pencil lead <br>");
         }
     }
 
     public function press()
     {
-        if($this->activate){
-            $this->activate = false;
-            print( "Off<br>");
-        } else{
-            $this->activate = true;
-            print( "On<br>");}
+        $this->activate = true; //push lead
+        print("Push<br>");
     }
 }
--- index.php
+++ index.php
@@ -4,10 +4,12 @@ function __autoload( $className ) {
     $className = str_replace( "..", "", $className );
     require_once( "Accessories/$className.php" );
 }
-//color, width line, body, description, manufacturer
-$pen = new Pen("blue", "0.7", "blue-black", "Useful series of pens with cap", "BRAUBERG");
+//width line, body, description, manufacturer
+$pen = new Pen("0.7", "blue-black", "Special protection against breakage of the lead.", "BRAUBERG");
 
 $pen->write($pen->getDescription());
-$pen->press();//on
+$pen->press();//push
 $pen->write($pen->getDescription());
-$pen->press();//off
+$pen->write($pen->getDescription());
+$pen->press();//push
+$pen->write($pen->getDescription());
